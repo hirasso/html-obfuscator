be12205268afa692e55bbeea0ba84ebc89ab0695
Rasso Hilber <mail@rassohilber.com>
2026-06-26 19:23:36 +0200

    add playwright-report-summary to CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests/e2e',
   outputDir: './tests/e2e/results',
+  reporter: [['list'], ['json', { outputFile: 'tests/e2e/results/report.json' }]],
   use: {
     baseURL: 'http://localhost:8080',
   },
