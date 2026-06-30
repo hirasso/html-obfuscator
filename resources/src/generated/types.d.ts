9a8a595dd5e17c93973a8dbee4dd2350f2486495
Rasso Hilber <mail@rassohilber.com>
2026-06-30 21:25:58 +0200

    Format generated types using Prettier

--- resources/src/generated/types.d.ts
+++ resources/src/generated/types.d.ts
@@ -1,11 +1,13 @@
 declare namespace Hirasso {
-namespace HTMLObfuscator {
-export type ScriptSettings = {
-debug: boolean,
-tagName: string,
-};
-namespace Enum {
-export type Regex = '[^\\s@]+@[^\\s@]+\\.[^\\s@]{2,}' | '[\\+\\d][\\d \\-\\(\\)\\.]{6,20}(?<!\\s)';
-}
-}
+  namespace HTMLObfuscator {
+    export type ScriptSettings = {
+      debug: boolean;
+      tagName: string;
+    };
+    namespace Enum {
+      export type Regex =
+        | "[^\\s@]+@[^\\s@]+\\.[^\\s@]{2,}"
+        | "[\\+\\d][\\d \\-\\(\\)\\.]{6,20}(?<!\\s)";
+    }
+  }
 }
