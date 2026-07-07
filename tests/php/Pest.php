01ed8f033842b3e3fc58d8ab5c23de95981875c3
Rasso Hilber <mail@rassohilber.com>
2026-07-07 15:41:06 +0200

    Restructure the tests folders

--- tests/php/Pest.php
+++ tests/php/Pest.php
@@ -2,7 +2,7 @@
 
 use Hirasso\HTMLObfuscator\HTMLObfuscator;
 
-require_once(dirname(__DIR__) . '/vendor/autoload.php');
+require_once(dirname(__DIR__, 2) . '/vendor/autoload.php');
 
 
 function dumpEnvironment(): void
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
-    include: ["tests/Unit/**/*.test.ts"],
-    globalSetup: ["tests/globalSetup.ts"],
+    include: ["tests/vitest/**/*.test.ts"],
+    globalSetup: ["tests/vitest/globalSetup.ts"],
   },
 });
